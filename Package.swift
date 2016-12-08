c562d27d4ad59b4553a5fa4f14431b1c1c73a16f
Jonathan Guthrie <jono@perfect.org>
2016-12-08 14:33:39 -0500

    uptick for most recent StORM as well as add linux exclusions

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-02 21:09:41 +0000
+// Date: 2016-12-08 19:13:59 +0000
 import PackageDescription
 let package = Package(
     name: "CouchDBStORM",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-CouchDB.git", majorVersion: 0, minor: 0),
-        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
         .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
     ]

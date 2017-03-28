f1d54f3df551171a5b6e658a62e7f03fed614678
Jonathan Guthrie <jono@perfect.org>
2017-03-28 11:07:07 -0400

    update to use major version only

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ let package = Package(
     name: "CouchDBStORM",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-CouchDB.git", majorVersion: 0, minor: 0),
-        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-CouchDB.git", majorVersion: 0),
+        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
+        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
     ]
 )
